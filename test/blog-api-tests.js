42a3190668451b680fbe5af01e741efa67c06efd
Race Briggs <race.briggs@gmail.com>
2018-11-12 15:22:39 -0600

    added other CRUD tests, still issues with POST

--- test/blog-api-tests.js
+++ test/blog-api-tests.js
@@ -33,6 +33,59 @@ describe("BlogPosts", function(){
 			})
 	});
 
+	//testing POST functionality
+	it('should append a new blog post into the collection of posts', function() {
+		const newPost = {title: 'test post', content: 'test content', author: 'author', publishDate: '11-12-2018'};
+
+		return (
+			chai.request(app)
+			.post('/blog-posts')
+			//.set('Accept', 'application/json')
+			.send(newPost)
+			.then(function(res) {
+					expect(res).to.have.status(201);
+					expect(res).to.be.json;
+					expect(res).to.be.a('object');
+					expect(res).to.include.keys("id", "content", "title", "author", "publishDate");
+					expect(res.body.id).to.not.equal(null);
+					expect(res.body).to.deep.equal(
+						Object.assign(newItem, {id: res.body})
+					);
+			})
+			);
+	});
+
+	//testing PUT functionality
+	it('should retrieve a GET request, then update a post with a matching id', function(){
+		const updatedPost = {title: 'new title', content: 'new content', author: 'new author', publishDate: '11-13-18'};
+
+		return (chai.request(app)
+			.get('/blog-posts')
+			.then(function(res){
+				updatedPost.id = res.body[0].id;
+				return chai.request(app)
+					.put(`/blog-posts/${updatedPost.id}`)
+					.send(updatedPost)
+			})
+			.then(function(res){
+				expect(res).to.have.status(204);
+				expect(res.body).to.deep.equal({});
+			})
+			);
+	});
+
+
+	//testing DELETE functionality
+	it('should remove a blog post by a matching id', function(){
+		return chai.request(app)
+			.get('/blog-posts')
+			.then(function(res){
+				return chai.request(app).delete(`/blog-posts/${res.body[0].id}`)
+			})
+			.then(function(res){
+				expect(res).to.have.status(204);
+			})
+	})
 
 
 
